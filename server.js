12acc1ee27621ab16a90e2cdc3880bd12dd1470b
Rafael Reyes <90874743+Rafaelreyes05@users.noreply.github.com>
2022-06-25 15:20:00 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+console.clear()
+const express = require('express')
+const fs = require('fs')
+
+const app = express ()
+const PORT = 8080
+
+
+class Container{
+    constructor(name){
+        this.name = name
+    }
+
+    getAll(){
+        let data = fs.readFileSync(this.name,'utf-8')
+        let productos = JSON.parse (data)
+        return productos
+    }
+
+    getRandom(){
+        let productos = this.getAll()
+        let number = Math.floor(Math.random() * productos.length)+1
+        console.log(number)
+        let productoRamdom = productos.find(producto=>producto.id===number)
+        console.log(productoRamdom)
+        return productoRamdom
+    }
+}
+
+const archivo = new Container ('productos.txt')
+
+
+app.get('/productos',(req,res)=>{
+    console.log('Todos los productos')
+    let productos = archivo.getAll()
+    res.send(productos)
+})
+
+app.get('/productoramdom',(req,res)=>{
+    console.log('Producto random')
+    let producto = archivo.getRandom()
+    res.send(producto)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Escuchando en el puerto ${PORT}`)
+})
